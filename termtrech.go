5dee832a73e4cbde8511edc9f4fded456fd1a0e1
Tyler D Kropp <kroppt@purdue.edu>
2018-04-26 01:14:33 -0400

    Add tutorial player collisions and viewport centering

--- termtrech.go
+++ termtrech.go
@@ -5,26 +5,46 @@ import (
 )
 
 // Player represents entity information for the player.
-// Is equivalent to a termloop Entity.
+// Player holds Entity info, previous coordinates.
 type Player struct {
 	*tl.Entity
+	prevX int
+	prevY int
+	level *tl.BaseLevel
 }
 
 // Tick executes one game loop
 func (player *Player) Tick(event tl.Event) {
 	if event.Type == tl.EventKey { // Is it a keyboard event?
-		x, y := player.Position()
+		player.prevX, player.prevY = player.Position()
 		switch event.Key { // If so, switch on the pressed key.
 		case tl.KeyArrowRight:
-			player.SetPosition(x+1, y)
+			player.SetPosition(player.prevX+1, player.prevY)
 		case tl.KeyArrowLeft:
-			player.SetPosition(x-1, y)
+			player.SetPosition(player.prevX-1, player.prevY)
 		case tl.KeyArrowUp:
-			player.SetPosition(x, y-1)
+			player.SetPosition(player.prevX, player.prevY-1)
 		case tl.KeyArrowDown:
-			player.SetPosition(x, y+1)
+			player.SetPosition(player.prevX, player.prevY+1)
 		}
 	}
+}
+
+// Collide stops the player in a collision.
+func (player *Player) Collide(collision tl.Physical) {
+	// Check if it's a Rectangle we're colliding with
+	if _, ok := collision.(*tl.Rectangle); ok {
+		player.SetPosition(player.prevX, player.prevY)
+	}
+}
+
+// Draw the screen where the player is centered.
+func (player *Player) Draw(screen *tl.Screen) {
+	screenWidth, screenHeight := screen.Size()
+	x, y := player.Position()
+	player.level.SetOffset(screenWidth/2-x, screenHeight/2-y)
+	// We need to make sure and call Draw on the underlying Entity.
+	player.Entity.Draw(screen)
 }
 
 func main() {
@@ -37,7 +57,10 @@ func main() {
 	})
 	level.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
 
-	player := Player{tl.NewEntity(1, 1, 1, 1)}
+	player := Player{
+		Entity: tl.NewEntity(1, 1, 1, 1),
+		level:  level,
+	}
 	// Set the character at position (0, 0) on the entity.
 	player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
 	level.AddEntity(&player)
