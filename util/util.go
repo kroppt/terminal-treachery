f01759182d2937030cd0f63bccdba1f86f46ee9d
Tyler Kropp <kropptyler@gmail.com>
2018-04-29 18:45:08 -0400

    Add FatalLog utility function for debug logging

--- util/util.go
+++ util/util.go
@@ -1,1 +1,14 @@
 package util
+
+import (
+	"log"
+
+	termbox "github.com/nsf/termbox-go"
+)
+
+// FatalLog closes the game out immediately and prints the given error
+func FatalLog(e error) {
+	termbox.Flush()
+	termbox.Close()
+	log.Fatalln(e)
+}
